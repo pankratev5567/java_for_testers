abe7e1e282d90cfabec33f93ff0e30add5274905
artem pantratev <manateee556@gmail.com>
2026-06-17 22:02:37 -0700

    Задание 2.
    1. Создать класс Triangle для представления треугольников. Объекты этого класса должны содержать три свойства, которые соответствуют длинам сторон треугольника.
    
    2. Реализовать методы для вычисления периметра и площади треугольника (для вычисления площади можно использовать формулу Герона).
    
    3. Сделать тесты, которые проверяют, что реализованные методы правильно работают (на "хороших" данных, которые соответствуют корректным треугольникам).

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -2,4 +2,41 @@ package ru.stqa.geometry.figures;
 
 public class Triangle {
 
+    private final double sideA;
+
+    private final double sideB;
+
+    private final double sideC;
+
+    // Конструктор – сохраняет стороны
+
+    public Triangle(double sideA, double sideB, double sideC) {
+
+        this.sideA = sideA;
+
+        this.sideB = sideB;
+
+        this.sideC = sideC;
+
+    }
+
+    // Периметр
+
+    public double getPerimeter() {
+
+        return sideA + sideB + sideC;
+
+    }
+
+    // Площадь по формуле Герона
+
+    public double getArea() {
+
+        double p = getPerimeter() / 2;
+
+        return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+
+    }
+
 }
+
---
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -0,0 +1,46 @@
+package ru.stqa.geometry.figures;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+
+public class TriangleTests {
+
+    @Test
+
+    void canCalculateArea() {
+
+        Triangle t = new Triangle(3, 4, 5);
+
+        double area = t.getArea();
+
+        Assertions.assertEquals(6.0, area);
+
+    }
+
+    @Test
+
+    void canCalculatePerimeter() {
+
+        Triangle t = new Triangle(4, 5, 6);
+
+        double perimeter = t.getPerimeter();
+
+        Assertions.assertEquals(15.0, perimeter);
+
+    }
+    // при sideA = 2, sideB = 8, sideC = 12 такого треугольника не существует
+
+    @Test
+
+    void canCalculateAreaForEquilateral() {
+
+        Triangle t = new Triangle(6, 6, 6);
+
+        double expected = Math.sqrt(3) / 4 * 36;
+
+        Assertions.assertEquals(expected, t.getArea(), 0.0001);
+
+    }
+
+}
