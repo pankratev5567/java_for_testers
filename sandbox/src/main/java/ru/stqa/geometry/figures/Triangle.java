b8c88bf9e0aa41ee30c193f92790527621b71320
artem pantratev <manateee556@gmail.com>
2026-06-24 21:07:00 -0700

    Задание №3
    Доработайте конструктор в классе Triangle, чтобы он выбрасывал исключение при попытке создать треугольник с отрицательной длиной стороны, а также в том случае, если нарушается неравенство треугольника (сумма двух любых сторон должна быть не меньше третьей стороны).
    
    Не забудьте сделать тесты, которые проверяют правильность работы созданного конструктора.

--- sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Triangle.java
@@ -1,41 +1,36 @@
 package ru.stqa.geometry.figures;
 
 public class Triangle {
-
     private final double sideA;
-
     private final double sideB;
-
     private final double sideC;
 
-    // Конструктор – сохраняет стороны
-
     public Triangle(double sideA, double sideB, double sideC) {
+        //проверка на отрицательные длины сторон
+        if (sideA <= 0 || sideB <= 0 || sideC <= 0) {
+            throw new IllegalArgumentException("Triangle side should not be negative");
+        }
 
+        //проверка неравенства треугольника
+        if (sideA + sideB < sideC || sideA + sideC < sideB || sideB + sideC < sideA) {
+            throw new IllegalArgumentException(
+                    "The sum of any two sides must be greater than or equal to the third side"
+            );
+        }
         this.sideA = sideA;
-
         this.sideB = sideB;
-
         this.sideC = sideC;
-
     }
 
-    // Периметр
-
+    //периметр
     public double getPerimeter() {
-
         return sideA + sideB + sideC;
-
     }
 
-    // Площадь по формуле Герона
-
+    //площадь по формуле Герона
     public double getArea() {
-
         double p = getPerimeter() / 2;
-
         return Math.sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
-
     }
 
 }
--- sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
+++ sandbox/src/test/java/ru/stqa/geometry/figures/TriangleTests.java
@@ -7,40 +7,49 @@ import org.junit.jupiter.api.Test;
 public class TriangleTests {
 
     @Test
-
     void canCalculateArea() {
-
         Triangle t = new Triangle(3, 4, 5);
-
         double area = t.getArea();
-
         Assertions.assertEquals(6.0, area);
 
     }
 
     @Test
-
     void canCalculatePerimeter() {
-
         Triangle t = new Triangle(4, 5, 6);
-
         double perimeter = t.getPerimeter();
-
         Assertions.assertEquals(15.0, perimeter);
 
     }
     // при sideA = 2, sideB = 8, sideC = 12 такого треугольника не существует
 
     @Test
-
-    void canCalculateAreaForEquilateral() {
-
+    void canCalculateAreaForSameSides() {
         Triangle t = new Triangle(6, 6, 6);
-
         double expected = Math.sqrt(3) / 4 * 36;
-
         Assertions.assertEquals(expected, t.getArea(), 0.0001);
 
     }
 
+    @Test
+    void cannotCreateTriangleWithNegativeSide() {
+        try {
+            new Triangle(-1.0, 2.0, 3.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+
+        }
+    }
+
+    @Test
+    void cannotCreateTriangleWithViolatedInequality() {
+        try {
+            new Triangle(2.0, 3.0, 6.0);
+            Assertions.fail();
+        } catch (IllegalArgumentException exception) {
+        }
+    }
 }
+
+
+
