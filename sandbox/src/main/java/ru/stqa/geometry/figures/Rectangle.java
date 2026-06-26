3d569d33521efefa58cccbe013a3e97857e0cca7
artem pantratev <manateee556@gmail.com>
2026-06-25 22:40:48 -0700

    Сравнение объектов (метод equals)

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,5 +1,7 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle(double a, double b) {
     public Rectangle {
         if (a < 0 || b < 0) {
@@ -10,6 +12,20 @@ public record Rectangle(double a, double b) {
     public static void printRectangleArea(double a, double b) {
         var text = String.format("Площадь прямоугольника со сторонами %f и %f = %f", a , b , rectangleArea(a,b));
         System.out.println(text);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(rectangle.a, this.a) == 0 && Double.compare(rectangle.b, this.b) == 0)
+                || (Double.compare(rectangle.a, this.b) == 0 && Double.compare(rectangle.b, this.a) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
     }
 
     private static double rectangleArea(double a, double b) {
