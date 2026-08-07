c507cab738370ccb3877696b34733f4a4ef503aa
artem pantratev <manateee556@gmail.com>
2026-08-08 00:35:00 +0300

    Задание №17: Подготовить новый проект для нового учебного приложения

---
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/ApplicationManager.java
@@ -0,0 +1,43 @@
+package ru.stqa.mantis.manager;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.Properties;
+
+public class ApplicationManager {
+
+    private WebDriver driver;
+    private String browser;
+    private Properties properties;
+    private SessionHelper sessionHelper;
+
+    public void init(String browser, Properties properties) {
+        this.browser = browser;
+        this.properties = properties;
+    }
+    public WebDriver driver(){
+        if (driver==null){
+            if ("firefox".equals(browser)){
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(browser)) {
+                driver = new ChromeDriver();
+            }
+            else {
+                throw new IllegalArgumentException(String.format("Unknown browser %s", browser));
+            }
+            Runtime.getRuntime().addShutdownHook(new Thread(driver::quit));
+            driver.get(properties.getProperty("web.baseURL"));
+            driver.manage().window().setSize(new Dimension(1936, 1048));
+        }
+        return driver;
+    }
+    public SessionHelper session(){
+        if (sessionHelper==null){
+            sessionHelper=new SessionHelper(this);
+        }
+        return sessionHelper;
+    }
+}
