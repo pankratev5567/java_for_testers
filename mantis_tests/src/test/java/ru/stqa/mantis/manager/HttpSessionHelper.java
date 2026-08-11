d5293400a0421378dfa33a7eeaf5176e3308699f
artem pantratev <manateee556@gmail.com>
2026-08-11 13:45:05 +0300

    Взаимодействие с сервером по протоколу HTTP

--- mantis_tests/src/test/java/ru/stqa/mantis/manager/HttpSessionHelper.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/manager/HttpSessionHelper.java
@@ -1,7 +1,10 @@
 package ru.stqa.mantis.manager;
 
 
-import okhttp3.OkHttpClient;
+import okhttp3.*;
+
+import java.io.IOException;
+import java.net.CookieManager;
 
 public class HttpSessionHelper extends HelperBase {
 
@@ -10,13 +13,35 @@ public class HttpSessionHelper extends HelperBase {
 
     public HttpSessionHelper(ApplicationManager manager) {
         super(manager);
-        client = new OkHttpClient();
+        client = new OkHttpClient.Builder().cookieJar(new JavaNetCookieJar(new CookieManager())).build();
     }
 
     public void login(String username, String password) {
+        RequestBody formBody = new FormBody.Builder()
+                .add("username", username)
+                .add("password", password)
+                .build();
+        Request request = new Request.Builder()
+                .url(String.format("%s/login.php",manager.getProperties("web.baseURL")))
+                .post(formBody)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new RuntimeException("Unexpected code " + response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public boolean isLoggedIn() {
-        return false;
+        Request request = new Request.Builder()
+                .url(manager.getProperties("web.baseURL"))
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new RuntimeException("Unexpected code " + response);
+            String body = response.body().string();
+            return body.contains("<span class=\"user-info\">");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
