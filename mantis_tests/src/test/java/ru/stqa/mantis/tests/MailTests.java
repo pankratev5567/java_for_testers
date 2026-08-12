ed32c76e468e0f2fed46158e730e7a789cdf1aad
artem pantratev <manateee556@gmail.com>
2026-08-12 13:46:06 +0300

    Извлечение ссылки из письма и удаление писем из ящика

--- mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/MailTests.java
@@ -3,14 +3,32 @@ package ru.stqa.mantis.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class MailTests extends TestBase{
+import java.time.Duration;
+import java.util.regex.Pattern;
+
+public class MailTests extends TestBase {
+    @Test
+    void canDrainInbox() {
+        app.mail().drain("user1@localhost", "password");
+    }
 
     @Test
-    void canReceiveEmail(){
-        var messages = app.mail().receive("user1@localhost","password");
+    void canReceiveEmail() {
+        var messages = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
         Assertions.assertEquals(1, messages.size());
         System.out.println(messages);
 
     }
 
+    @Test
+    void canExtractUrl() {
+        var message = app.mail().receive("user1@localhost", "password", Duration.ofSeconds(60));
+        var text = message.get(0).content();
+        var pattern = Pattern.compile("http://\\S+");
+        var matcher = pattern.matcher(text);
+        if (matcher.find()) {
+            var url = text.substring(matcher.start(), matcher.end());
+            System.out.println(url);
+        }
+    }
 }
---
+++ mantis_tests/src/test/java/ru/stqa/mantis/tests/UserRegistrationTests.java
@@ -0,0 +1,17 @@
+package ru.stqa.mantis.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRegistrationTests extends TestBase{
+
+    @Test
+    void canRegisterUser(){
+        //var email = String.format("%s@localhost", username);
+//          Создать пользователя (адрес) на почтовом сервере (JamesHelper)
+//          Открыть браузер и заполнить форму создания и отправляем (браузер)
+//          ждём почту  (MailHelper)
+//          извлекаем ссылку из письма
+//          проходим по ссылке и завершаем регестрацию (браузер)
+//          проверяем что пользователь может залогиниться (HttpSessionHelper)
+    }
+}
